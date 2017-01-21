03ce3d105cc538233eceb6820cc885bfaa966234
Bjoern Stiel <bjoern.stiel@pathio.com>
2017-01-21 15:00:57 +0000

    Removed level from log message

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s",
+    format="%(asctime)s %(message)s",
     handlers=[logging.StreamHandler()]
 )
 
@@ -16,16 +16,16 @@ SERVER_PORT = os.environ.get('UDP_SERVER_PORT', 10000)
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     logging.info("Socket created")
-except socket.error:
-    logging.critical("Failed to create socket. Error code is '%s'. Message is '%s'" % (str(msg[0]), msg[1]))
+except socket.error as ex:
+    logging.error(ex)
     sys.exit(2)
 
 
 try:
     s.bind((SERVER_HOST, SERVER_PORT))
     logging.info("Socket successfully binded")
-except socket.error:
-    logging.critical("Bind failed. Error code is '%s'. Message is '%s'" % (str(msg[0]), msg[1]))
+except socket.error as ex:
+    logging.critical(ex)
     sys.exit(2)
 
 while 1:
