b1a518b3550ba6e0eb59035418cd5bf3912b8136
Bjoern Stiel <bjoern.stiel@pathio.com>
2017-01-21 12:47:18 +0000

    Added README

---
+++ README.md
@@ -0,0 +1,9 @@
+UDP SERVER
+==========
+
+Simple UDP server
+Use case: collect log messages and write to stdout
+
+Docker image
+==========
+https://hub.docker.com/r/zoomeranalytics/udp-server/
--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 import os
-import re
 import sys
-import json
 import socket
 import logging
 
-from datetime import datetime
-
-is_in_debug_mode = os.getenv('UDP_SERVER_DEBUG', 0)
-debug_mode_state = 'ON' if is_in_debug_mode else 'OFF'
 
 logging.basicConfig(
-    level=logging.DEBUG if is_in_debug_mode else logging.INFO,
-    filename="/var/log/udp-server.log",
-    format="%(asctime)s %(levelname)s %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    handlers=[logging.StreamHandler()]
 )
 
-SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 1234
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = os.environ['UDP_SERVER_PORT']
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,40 +28,12 @@ except socket.error:
     logging.critical("Bind failed. Error code is '%s'. Message is '%s'" % (str(msg[0]), msg[1]))
     sys.exit(2)
 
-logging.info("Server has started successfully and operating on host %s and port %s" % (SERVER_HOST, SERVER_PORT))
-logging.info("DEBUG mode is %s", debug_mode_state)
-
 while 1:
-    d = s.recvfrom(1024)
-    data = d[0] # data
-    addr = d[1] # client's address
+    package = s.recvfrom(1024)
+    message = package[0]
+    address = package[1]
 
-    if not data:
-        break
-
-    #[17/06/2016 12:30] Time to move
-    # message = re.match(r'\[(?P<event_datetime>.*)\] (?P<event_message>.*)', data)
-    message = data
-
-    if message is None:
-        error_message = "Error: wrong message format. Must be '[DD/MM/YYYY HH:MM] <MESSAGE BODY>'\n"
-        logging.error(error_message)
-    else:
-        logging.debug("Received a message. Sender's address is '%s', message is '%s'" % (addr, data))
-
-        message_dict = message.groupdict()
-        try:
-            timestamp = datetime.strptime(message_dict['event_datetime'], '%d/%m/%Y %H:%M').strftime("%s")
-        except Exception as e:
-            error_message = "Can't convert the given datetime '%s' to timestamp." % message_dict['event_datetime']
-            logging.error(error_message)
-
-        else:
-            result = {}
-            result['timestamp'] = timestamp
-            result['message'] = message_dict['event_message']
-            sys.stdout.write(json.dumps(result) + '\n')
-
-            logging.debug("Client's address is '%s', result is '%s'" % (addr, result))
+    if message:
+        logging.info('%s [%s]' % (message.decode('utf-8'), address[0]))
 
 s.close()
